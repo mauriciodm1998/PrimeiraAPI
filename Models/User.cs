1fe0e6bbfbc577760776068c3ac2f35add47c013
Mauricio <mauriciodmpires1@gmail.com>
2021-02-23 16:41:36 -0300

    API finalizada

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Models
+{
+    public class User 
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter no máximo 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter no minimo 3 caracteres")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter no máximo 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter no minimo 3 caracteres")]
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
